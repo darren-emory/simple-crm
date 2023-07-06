b3d32105ef269c32d4e801fb10e39eafc4fac76e
Darren Emory <darren.emory@inspireinvesting.com>
2023-07-06 14:40:14 -0600

    build

--- src/main.tsx
+++ src/main.tsx
@@ -90,6 +90,18 @@ const router = createBrowserRouter([
           </motion.div>
         ),
       },
+      {
+        path: "",
+        element: (
+          <motion.div
+            initial={{ opacity: 0 }}
+            animate={{ opacity: 1 }}
+            transition={{ duration: 0.5 }}
+          >
+            <Dashboard />
+          </motion.div>
+        ),
+      },
     ],
   },
 ]);
